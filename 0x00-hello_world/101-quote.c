39de9c5555d53f6b0f752265a5f02ce638b613fa
Est-1y <terryiana22@gmail.com>
2023-06-15 11:58:01 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
