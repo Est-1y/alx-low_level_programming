6400a7e50369d010221c86fb28afc30b88995d40
Est-1y <terryiana22@gmail.com>
2023-06-15 09:19:24 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	print("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+print("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
