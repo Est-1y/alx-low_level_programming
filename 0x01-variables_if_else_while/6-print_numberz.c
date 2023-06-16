e9363e2c940813d24adf7bc71cec71d32eb3e91f
Est-1y <terryiana22@gmail.com>
2023-06-16 04:52:22 -0700

    numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
