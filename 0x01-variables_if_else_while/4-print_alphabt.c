babc4209c2d5e50e140416be12a74aea1b2e37ce
Est-1y <terryiana22@gmail.com>
2023-06-16 04:39:19 -0700

    alphbt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+if (lowerCase == 'e' || lowerCase == 'q')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+}
+putchar('\n');
+return (0);
+}
+
