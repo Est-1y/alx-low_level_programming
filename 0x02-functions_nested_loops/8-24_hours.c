8bddb0ccdfbaa60b1376dcc4568f4e7f3d63f72c
Est-1y <terryiana22@gmail.com>
2023-06-20 11:36:26 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,13 +11,13 @@ void jack_bauer(void)
 
 int a, b, c, d;
 
-for (a =0; a <= 2; a++)
+for (a = 0; a <= 2; a++)
 {
-for (b =0; b <= 9; b++)
+for (b = 0; b <= 9; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
-for (c =0; c <= 5; c++)
+for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 {
