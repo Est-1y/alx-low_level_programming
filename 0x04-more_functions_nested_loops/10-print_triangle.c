1f7d4e9235023f714a74d5c14fa51b35618dc785
Est-1y <terryiana22@gmail.com>
2023-06-22 16:42:42 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry
+ * Description: i prints diagonals
+ * @size: void
+ */
+
+void print_triangle(int size)
+{
+int row, hashes, spaces;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (spaces = size - row; spaces >= 1; spaces--)
+{
+_putchar(' ');
+}
+for (hashes = 1; hashes <= row; hashes++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
