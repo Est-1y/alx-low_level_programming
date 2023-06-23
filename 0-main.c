7a7b950cf654b08f56c895bcbeb62f394a121fd1
Est-1y <terryiana22@gmail.com>
2023-06-22 18:04:46 -0700

    debug

--- 0-main.c
+++ 0-main.c
@@ -1,4 +1,3 @@
-carrie@ubuntu:/debugging$ cat main.c
 #include "main.h"
 
 /**
@@ -8,10 +7,8 @@ carrie@ubuntu:/debugging$ cat main.c
 
 int main(void)
 {
-        int i;
-
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
