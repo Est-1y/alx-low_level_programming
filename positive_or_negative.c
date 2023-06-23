e71602fa7026c2474976e18da5eef0c329a496d8
Est-1y <terryiana22@gmail.com>
2023-06-22 18:57:07 -0700

    rem

--- positive_or_negative.c
+++ positive_or_negative.c
@@ -1,17 +1,26 @@
 #include "main.h"
+/**
+ * main - determine if a number is positive or negative
+ * positive_or_negative: determine if int i is positive or negative
+ *@i: integer
+ * Return: void
+ */
 
 void positive_or_negative(int i)
 {
 	if (i > 0)
 	{
 		printf("%d is positive\n", i);
+		return (1);
 	}
 	else if (i < 0)
 	{
 		printf("%d is negative\n", i);
+		return (-1);
 	}
 	else
 	{
 	printf("%d is zero\n", i);
+	return (0);
 	}
 }
