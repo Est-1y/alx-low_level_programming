80682008cef4e9e30bd40230e72ba06a936d4a1e
Est-1y <terryiana22@gmail.com>
2023-06-25 10:58:08 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 /**
-* while (i < 10)
+* while (i < 10) - explains the integer i
 *{
 *putchar(i);
 *}
+*\\o/\n: avoid infinite loop
+*@closing speech mark: closes the infinite loop speech
 */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
+
 }
