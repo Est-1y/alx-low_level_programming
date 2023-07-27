16818a26ccae5d1f4aa5b82f8e3081f00fc5b7cd
Est-1y <terryiana22@gmail.com>
2023-07-27 03:15:07 -0700

    first

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-printf("(A tortoise, having pretty good sense of a hare's nature,
-	challenges one to a race.)\n");
+printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 return (0);
 }
