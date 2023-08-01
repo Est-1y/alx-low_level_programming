e390a08c47ff09dd2ff5ed495ebddf3747c01752
Est-1y <terryiana22@gmail.com>
2023-08-01 08:08:07 -0700

    listint len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - it returns elements no in a linked lists
+ * @h: linked list to traverse
+ * Return: the number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+}
+
+return (num);
+}
--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -12,7 +12,7 @@ int main(void)
 {
 listint_t *head;
 listint_t *new;
-istint_t hello = {8, NULL};
+listint_t hello = {8, NULL};
 size_t n;
 
 head = &hello;
