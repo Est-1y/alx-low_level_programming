7dce93e708ad1c35cc45985af372b625e93a27e2
Est-1y <terryiana22@gmail.com>
2023-11-04 22:43:33 -0700

    tab delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,25 +6,29 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+	unsigned long int index;
+	hash_node_t *node, *next_node;
 
-	for (i = 0; i < ht->size; i++)
+	if (ht == NULL)
+		return;
+
+	index = 0;
+	while (index < ht->size)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[index];
+		while (node != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+			next_node = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			if (next_node != NULL)
+				node = next_node;
+			else
+				break;
 		}
+		index++;
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
