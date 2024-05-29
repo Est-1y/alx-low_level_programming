e0ea84e00da03d58355794c2442c133241d51a71
Est-1y <terryiana22@gmail.com>
2024-05-29 06:56:20 -0700

    makefile 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,18 +9,18 @@ def island_perimeter(grid):
     Calculate the perimeter.
     """
     c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    length = len(grid)
+    width = len(grid[0])
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
+    for i in range(length):
+        for j in range(width):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
                     c += 1
-                if j == 0 or grid[i][j - 1] != 1:
+                if i == length - 1 or grid[i + 1][j] == 0:
                     c += 1
-                if j == width or grid[i][j + 1] != 1:
+                if j == 0 or grid[i][j - 1] == 0:
                     c += 1
-                if i == length or grid[i + 1][j] != 1:
+                if j == width - 1 or grid[i][j + 1] == 0:
                     c += 1
-                return c
+    return c
