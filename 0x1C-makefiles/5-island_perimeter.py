3ecafdf088b4d65033115414b25c6c1df47b3c5c
Est-1y <terryiana22@gmail.com>
2024-05-29 02:24:02 -0700

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Island Perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Based on the premise that a land cell adds 4 to the total
+                perimeter += 4
+
+        # Evaluate land adjacency
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,18 @@
+Learning Objectives
+
+General
+What are make, Makefiles
+When, why and how to use Makefiles
+What are rules and how to set and use them
+What are explicit and implicit rules
+What are the most common / useful rules
+What are variables and how to set and use them
+
+Requirements
+General
+Allowed editors: vi, vim, emacs
+OS: Ubuntu 20.04 LTS
+Version of gcc: 9.3.0
+Version of make: GNU Make 4.2.1
+All your files should end with a new line
+A README.md file, at the root of the folder of the project, is mandatory
