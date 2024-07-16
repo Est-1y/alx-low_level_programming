8060393dc3b6356633ac0a0226fb4b50cbc63575
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-07-16 15:19:02 +0300

    Create create_skiplist.c

---
+++ 0x1E-search_algorithms/skiplist/create_skiplist.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+
+void free_skiplist(skiplist_t *list);
+
+/**
+ * init_express - it activates linked list express lane
+ * @list: Points to the list head node
+ * @size: size of nodes
+ */
+void init_express(skiplist_t *list, size_t size)
+{
+	const size_t step = sqrt(size);
+	size_t i;
+	skiplist_t *save;
+
+	for (save = list, i = 0; i < size; ++i, list = list->next)
+	{
+		if (i % step == 0)
+		{
+			save->express = list;
+			save = list;
+		}
+	}
+}
+
+/**
+ * create_skiplist - it creates a single linked list.
+ * @array: Points to the array
+ * @size: Size of the array
+ * Return: (A pointer to the head of the list created) or (NULL on failure)
+ */
+skiplist_t *create_skiplist(int *array, size_t size)
+{
+	skiplist_t *list;
+	skiplist_t *node;
+	size_t save_size;
+
+	list = NULL;
+	save_size = size;
+	while (array && size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			free_skiplist(list);
+			return (NULL);
+		}
+		node->n = array[size];
+		node->index = size;
+		node->express = NULL;
+		node->next = list;
+		list = node;
+	}
+	init_express(list, save_size);
+	return (list);
+}
